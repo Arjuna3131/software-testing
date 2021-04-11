d764a6784558f99b321bdee723b89a75443106aa
Arjuna3131 <49694103+Arjuna3131@users.noreply.github.com>
2021-04-11 10:58:30 +0530

    Update test10.py

--- Unit testing/test10.py
+++ Unit testing/test10.py
@@ -16,7 +16,7 @@ class Test10(unittest.TestCase):
     
     def test_10(self):
         driver = self.driver
-        driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login")
+        driver.get("https://opensource-demo.blueehrmlive.com/index.php/auth/login")
         driver.find_element_by_id("txtUsername").clear()
         driver.find_element_by_id("txtUsername").send_keys("Admin")
         driver.find_element_by_id("txtPassword").clear()
